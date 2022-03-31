0cec8f718af4e5a10eb2ccd6a90d6236b23d8ee1
Guilherme Monteiro <dkglima@gmail.com>
2022-03-30 22:01:17 -0300

    feat: add devise module cofirmable and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :exercises
   resources :routines
-  devise_for :users
+  devise_for :users, controllers: { confirmations: 'confirmations' }
   root 'routines#index'
 end
